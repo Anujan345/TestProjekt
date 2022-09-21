37a97ffeb0ffa514e09a82f1d9467ddffa93045e
anujan <eaaanus@students.eaaa.dk>
2022-09-21 12:29:34 +0200

    Version 2 DagligSkaev

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,48 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
 
+    ArrayList<Dosis> doses = new ArrayList<>();
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
 
+    public void opretDosis(LocalTime[] tid, double[] antal) {
+        for (int i = 0; i < tid.length; i++) {
+            for (int j = 0; j < antal.length; j++) {
+                doses.add(new Dosis(tid[i],antal[j]));
+                i++;
+                j++;
+            }
+        }
+    }
 
-    public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+    @Override
+    public double samletDosis() {
+        int dage = (int) ChronoUnit.DAYS.between(getStartDen(), getSlutDen());
+        double sum =0;
+        for (Dosis d : doses){
+            sum += d.getAntal();
+        }
+        return sum * dage;
+    }
+
+    @Override
+    public double doegnDosis() {
+        double sum =0;
+        for (Dosis d : doses){
+            sum += d.getAntal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig skæv";
     }
 }
