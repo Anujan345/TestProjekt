2952fd11ffcc155029384043d90c863e5ec1c674
anujan <eaaanus@students.eaaa.dk>
2022-09-23 12:30:30 +0200

    Version 14 GUI

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -73,12 +73,43 @@ Laegemiddel laegemiddel;
         controller = Controller.getController();
 
         //Act
-        DagligFast actual = controller.opretDagligFastOrdination(LocalDate.of(2002, 10, 10),
-                LocalDate.of(2002, 10, 20),patient,laegemiddel,2, 3, 4, 5);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> controller.opretDagligFastOrdination(LocalDate.of(2022, 9, 22),
+                LocalDate.of(2021, 9, 22),patient,laegemiddel,2, 3, 4, 5));
+        String expected = "Den er udenfor ordinationsdatoerne";
+
+        //Assert
+        assertEquals(expected,exception.getMessage() );
+    }
+
+    @Test
+    void TC2_opretDagligFastOrdination() {
+        //Arrange
+        patient = new Patient("111111","Anujan",20);
+        laegemiddel = new Laegemiddel("asd",10,20,30,"sa");
+        controller = Controller.getController();
+
+        //Act
+        DagligFast actual =controller.opretDagligFastOrdination(LocalDate.of(2022, 9, 22),
+                LocalDate.of(2022, 9, 23),patient,laegemiddel,2, 2, 0, 0);
         Ordination expected = patient.getOrdinations().get(0);
 
         //Assert
-        assertEquals(expected,actual );
+        assertEquals(expected,actual);
+    }
+    @Test
+    void TC3_opretDagligFastOrdination() {
+        //Arrange
+        patient = new Patient("111111","Anujan",20);
+        laegemiddel = new Laegemiddel("asd",10,20,30,"sa");
+        controller = Controller.getController();
+
+        //Act
+        DagligFast actual =controller.opretDagligFastOrdination(LocalDate.of(2022, 9, 22),
+                LocalDate.of(2022, 9, 25),patient,laegemiddel,3, 2, 1, 0);
+        Ordination expected = patient.getOrdinations().get(0);
+
+        //Assert
+        assertEquals(expected,actual);
     }
 
     @Test
