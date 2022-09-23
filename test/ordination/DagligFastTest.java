9da1ab94c9365d5e6732a3f31f4fce524055f8b3
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2022-09-23 10:12:54 +0200

    Mere Test!! :D

--- test/ordination/DagligFastTest.java
+++ test/ordination/DagligFastTest.java
@@ -1,46 +1,116 @@
 package ordination;
 
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligFastTest {
+    private Patient patient;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        this.patient = new Patient("1111111111", "Esben", 80);
     }
 
     @org.junit.jupiter.api.Test
     void morgen() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2020, 10, 1),LocalDate.of(2022, 10, 1),patient,null);
+
+        //Act
+        Dosis actual = dagligFast.morgen(2);
+        Dosis expected = (Dosis) Array.get(dagligFast.getDagligDosis(),0);
+
+        //Assert
+        assertEquals(expected, actual);
+//        System.out.println(actual);
+//        System.out.println(expected);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void middag() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2020, 10, 1),LocalDate.of(2022, 10, 1),patient,null);
+
+        //Act
+        Dosis actual = dagligFast.middag(2);
+        Dosis expected = (Dosis) Array.get(dagligFast.getDagligDosis(),1);
+
+        //Assert
+        assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
     void aften() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2020, 10, 1),LocalDate.of(2022, 10, 1),patient,null);
+
+        //Act
+        Dosis actual = dagligFast.aften(2);
+        Dosis expected = (Dosis) Array.get(dagligFast.getDagligDosis(),2);
+
+        //Assert
+        assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
     void nat() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2020, 10, 1),LocalDate.of(2022, 10, 1),patient,null);
+
+        //Act
+        Dosis actual = dagligFast.nat(2);
+        Dosis expected = (Dosis) Array.get(dagligFast.getDagligDosis(),3);
+
+        //Assert
+        assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
-    void samletDosis() {
+    void TC2_samletantal1_start_22_09_22_slut_23_09_22_samletDosis() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2022, 9, 22),LocalDate.of(2022, 9, 23),patient,null);
+        Dosis morgen = dagligFast.morgen(1);
+        Dosis middag = dagligFast.middag(1);
+        Dosis aften = dagligFast.aften(1);
+        Dosis nat = dagligFast.nat(1);
+        double actual = dagligFast.samletDosis();
+        double expected = 8;
+
+        //Assert
+        assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
-    void getDagligDosis() {
+    void TC2_samletantal2_start_22_09_22_slut_24_09_22_samletDosis() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2022, 9, 22),LocalDate.of(2022, 9, 24),patient,null);
+        Dosis morgen = dagligFast.morgen(2);
+        Dosis middag = dagligFast.middag(2);
+        Dosis aften = dagligFast.aften(2);
+        Dosis nat = dagligFast.nat(2);
+        double actual = dagligFast.samletDosis();
+        double expected = 24;
+
+        //Assert
+        assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
-    void getLaegemiddel() {
-    }
+    void TC3_samletantal3_start_22_09_22_slut_24_09_22_samletDosis() {
+        //Arrange
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2022, 9, 22),LocalDate.of(2022, 9, 22),patient,null);
+        Dosis morgen = dagligFast.morgen(3);
+        Dosis middag = dagligFast.middag(3);
+        Dosis aften = dagligFast.aften(3);
+        Dosis nat = dagligFast.nat(3);
+        double actual = dagligFast.samletDosis();
+        double expected = 12;
 
-    @org.junit.jupiter.api.Test
-    void doegnDosis() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getType() {
+        //Assert
+        assertEquals(expected, actual);
     }
 }
--- test/ordination/DagligSkaevTest.java
+++ test/ordination/DagligSkaevTest.java
@@ -1,14 +1,18 @@
 package ordination;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligSkaevTest {
+    private Patient patient;
 
-    @BeforeEach
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        this.patient = new Patient("1111111111", "Esben", 80);
     }
 
     @Test
@@ -25,10 +29,33 @@ class DagligSkaevTest {
 
     @Test
     void samletDosis() {
+        void TC1_samletantal3_start_22_09_22_slut_24_09_22_samletDosis() {
+            //Arrange
+            DagligFast dagligFast = new DagligFast(LocalDate.of(2022, 9, 22),LocalDate.of(2022, 9, 22),patient,null);
+            Dosis morgen = dagligFast.morgen(3);
+            Dosis middag = dagligFast.middag(3);
+            Dosis aften = dagligFast.aften(3);
+            Dosis nat = dagligFast.nat(3);
+            double actual = dagligFast.samletDosis();
+            double expected = 12;
+
+            //Assert
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
     void doegnDosis() {
+        void TC1_samletantal3_start_22_09_22_slut_24_09_22_samletDosis() {
+            //Arrange
+            DagligSkaev dagligSkaev = new DagligSkaev(LocalDate.of(2022, 9, 22),LocalDate.of(2022, 9, 22),patient,null);
+
+            double actual = dagligSkaev.doegnDosis();
+            double expected = 12;
+
+            //Assert
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
